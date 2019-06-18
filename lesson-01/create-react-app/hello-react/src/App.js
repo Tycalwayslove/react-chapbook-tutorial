3805e025a18aa57175c89a00b2374be6e99190c9
Tycalwayslove <1186446929@qq.com>
2019-06-18 14:54:04 +0800

    <feat> 了解react基础，熟悉组件树的构建形式

--- lesson-01/create-react-app/hello-react/src/App.js
+++ lesson-01/create-react-app/hello-react/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'
+// import ReactDOM from 'react-dom'
 import './App.css';
+class Header extends Component{
+  render(){
+    return (
+      <div>
+        <h1>我在学习react 小书</h1>
+      </div>
+    )
+  }
+}
+class LikeButton extends Component {
+  constructor(){
+    super()
+    this.state = {isLiked:false}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  handleClickOnLikeButton(){
+    this.setState({isLiked:!this.state.isLiked})
+  }
+  render(){
+    return (
+      <button onClick={this.handleClickOnLikeButton.bind(this)}>{this.state.isLiked?'取消':'点赞'}👍</button>
+    )
+  }
+}
+class Main extends Component {
+  handleClick(e){
+    console.log(this)
+    console.log(e.target.innerHTML)
+  }
+  render(){
+    return (
+      <div>
+        <h1 onClick={this.handleClick.bind(this)}>点击我试试</h1>
+        <LikeButton></LikeButton>
+      </div>  
+
+    )
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="container">
+        <Header/>
+        <Main/>
+        我是哈哈哈
+      </div>
+      )
+  }
 }
 
 export default App;
